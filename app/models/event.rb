3e86558b6b5ab4ecf266afc4077ed4edecb902b4
maressajoseph <maressa.joseph@gmail.com>
2017-05-29 12:42:43 +0200

    Create model Photo

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :registrations, dependent: :destroy
   has_many :guests, through: :registrations, source: :user
+  has_many :photos
 
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Theme.destroy_all
 User.destroy_all
 Event.destroy_all
 Registration.destroy_all
+Photo.destroy_all
 
 party = Theme.create(name: "Party")
 conference = Theme.create(name: "Conference")
@@ -16,9 +17,11 @@ maressa = User.create(email: "maressa@codaisseurup.com", password: "abcd1234")
 maressa1 = User.create(email: "maressa1@codaisseurup.com", password: "abcd1234")
 maressa2 = User.create(email: "maressa2@codaisseurup.com", password: "abcd1234")
 
+photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/maressajoseph/image/upload/v1496054207/drinks-1200x520_cygtte.jpg")
+
 event1 = Event.create(name: "VriMiBo", description: "Friday afternoon drinks", location: "TQ", price: 0.0,
 capacity: 50, includes_food: false, includes_drinks: true, starts_at: DateTime.now,
-ends_at: DateTime.now, active: true, user: maressa, themes: [drinks, party])
+ends_at: DateTime.now, active: true, user: maressa, themes: [drinks, party], photos: [photo1])
 
 event2 = Event.create(name: "Breakfast Meeting", description: "Breakfast Meeting", location: "TQ", price: 5.0,
 capacity: 6, includes_food: true, includes_drinks: true, starts_at: DateTime.now,
@@ -28,5 +31,5 @@ event3 = Event.create(name: "Walk + Lunch", description: "Walk + Lunch", locatio
 capacity: 10, includes_food: true, includes_drinks: true, starts_at: DateTime.now,
 ends_at: DateTime.now, active: true, user: maressa2, themes: [activity, lunch])
 
-Registration.create(event: event1, user: maressa, status: "Going", price: 100, guest_count: 1)
-Registration.create(event: event1, user: maressa1, status: "Going", price: 200, guest_count: 2)
+Registration.create(event: event1, user: maressa, status: "Going", price: 100, guests_count: 1)
+Registration.create(event: event1, user: maressa1, status: "Going", price: 200, guests_count: 2)
